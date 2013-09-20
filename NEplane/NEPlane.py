c76907df8894cb55b13153a01f26181053ccda42
Laurent FABRE <fabre@photonic-lattice.com>
2013-09-21 08:18:46 +0900

    Added the calculation of the total distance travelled by the plane.
    It seems to confirm the D=πR/2 / sinα formula (theory: 2.22144*R).

--- NEplane/NEPlane.py
+++ NEplane/NEPlane.py
@@ -1,16 +1,28 @@
 from numpy import arange
-from math import pi
-from math import sin
-from math import cos
+from math import pi, sin, cos, sqrt
+
+def geographicToCartesian(theta, phi):
+    return [ cos(phi) * cos(theta), cos(phi) * sin(theta), sin(phi) ]
+
+def distance(theta1, phi1, theta2, phi2):
+    p1 = geographicToCartesian(theta1, phi1)
+    p2 = geographicToCartesian(theta2, phi2)
+    return sqrt((p2[0]-p1[0])**2 + (p2[1]-p1[1])**2 + (p2[2]-p1[2])**2)
 
 def NEPlane(direction, step, start_theta = 0, start_phi = 0, filename = 'plot/NEPlane.dat'):
+    D = 0
     theta = start_theta
-    for phi in arange(start_phi, pi/2, step * sin(direction)):
+    old_phi = start_phi
+    old_theta = theta
+    for phi in arange(start_phi, pi/2, step * sin(direction)):        
         theta = theta + step * cos(direction) / cos(phi)
+        D = D + distance(old_theta, old_phi, theta, phi)
         if theta >= 2*pi:
             theta = theta - 2*pi
+        old_phi = phi
+        old_theta = theta
         writeResult(filename, theta*180/pi, phi*180/pi)
-        print("(" + str(phi*180/pi) + "," + str(theta*180/pi) + ")")
+        print("(" + str(phi*180/pi) + "," + str(theta*180/pi) + ") D=" + str(D))
 
 def writeResult(filename, theta, phi):
     f = open(filename, 'a')
