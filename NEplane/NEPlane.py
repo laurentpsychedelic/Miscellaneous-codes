de380ab157ecc6b4cd517ca0f831597cafa88881
Laurent FABRE <fabre@photonic-lattice.com>
2013-09-21 15:19:38 +0900

    Added an assertion verifying the relation D=Rπ/2 / sinα.

--- NEplane/NEPlane.py
+++ NEplane/NEPlane.py
@@ -1,4 +1,4 @@
-from numpy import arange
+from numpy import arange, allclose
 from math import pi, sin, cos, sqrt
 
 def geographicToCartesian(theta, phi):
@@ -23,13 +23,15 @@ def NEPlane(direction, step, start_theta = 0, start_phi = 0, filename = 'plot/NE
         old_theta = theta
         writeResult(filename, theta*180/pi, phi*180/pi)
         print("(" + str(phi*180/pi) + "," + str(theta*180/pi) + ") D=" + str(D))
+    return D
 
 def writeResult(filename, theta, phi):
     f = open(filename, 'a')
     f.write(str(theta) + '\t' + str(phi) + '\n')
 
 if __name__ == '__main__':
-    NEPlane(pi/4, pi/10000)
+    D = NEPlane(pi/4, pi/10000)
+    assert allclose(D, pi/2 / sin(pi/4), atol = 0.01), "Distance calculated different from predicted! " + str(D) + "!=" + str(pi/2 / sin(pi/4))
     NEPlane(pi/4, pi/1000000, start_theta = 498.591563012*pi/180, start_phi = 89.9736810518*pi/180, filename = 'plot/NEPlane_detail.dat')
     NEPlane(pi/4, pi/1000000000, start_theta = 862.027255904*pi/180, start_phi = 89.9999005712*pi/180, filename = 'plot/NEPlane_detail2.dat')
 
