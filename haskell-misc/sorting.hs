e39b21a5bd56e1a1510a7f00df2f1e3ae62456ed
Laurent FABRE <laurent_psychedelic@hotmail.com>
2013-12-23 21:31:48 +0900

    Added a (naive) heap sort implementation to the benchmark.

--- haskell-misc/sorting.hs
+++ haskell-misc/sorting.hs
@@ -5,23 +5,24 @@ import Data.Tuple
 
 main = do
   putStrLn( "List:\t " ++ showPartial 10 ( xs ) )
-  putStrLn( "  Sorted? -> " ++ show( isSorted xs ) )
   putStrLn( "<<Stree>>" )
-  -- putStrLn( "\tTree:\t " ++ show( yt ) )
-  -- putStrLn( "\tFlat:\t " ++ show( flattenS yt ) )
   timeIt( putStrLn( "\tSorted:\t " ++ showPartial 10 ( sortS xs ) ) )
-  putStrLn( "  Sorted? -> " ++ show( isSorted ys ) )
+  putStrLn( "  Sorted? -> " ++ show( isSorted as ) )
   putStrLn( "Benchmark:" )
   sequence_ [ timeIt( putStrLn( (showPartial 1) ( sortS( getRandomArray n 0 1000 ) ) ) ) | n <- ns ]
+  putStrLn( "<<Htree>>" )
+  timeIt( putStrLn( "\tSorted:\t " ++ showPartial 10 ( sortH xs ) ) )
+  putStrLn( "  Sorted? -> " ++ show( isSorted bs ) )
+  putStrLn( "Benchmark:" )
+  sequence_ [ timeIt( putStrLn( (showPartial 1) ( sortH( getRandomArray n 0 1000 ) ) ) ) | n <- ns ]
   putStrLn( "<<BubbleSort>>" )
   timeIt( putStrLn( "\tSorted:\t " ++ showPartial 10 ( bubbleSort xs ) ) )
-  putStrLn( "  Sorted? -> " ++ show( isSorted zs ) )
+  putStrLn( "  Sorted? -> " ++ show( isSorted cs ) )
   putStrLn( "Benchmark:" )
   sequence_ [ timeIt( putStrLn( (showPartial 1) ( bubbleSort( getRandomArray n 0 1000 ) ) ) ) | n <- ns ]
-  
-    where ys = sortS xs
-          -- yt = mkStree xs
-          zs = bubbleSort xs
+    where as = sortS xs
+          bs = sortH xs
+          cs = bubbleSort xs
           xs = getRandomArray 1000 0 1000
           ns = [ 100,200..2000 ]
 
@@ -40,18 +41,51 @@ data Stree a = Null | ForkS (Stree a) a (Stree a)
 instance (Show a) => Show (Stree a) where
   show = showStree
 
+data Htree a = NullH | ForkH a (Htree a) (Htree a)
+instance (Show a) => Show (Htree a) where
+  show = showHtree
+
 mkStree :: (Ord a) => [ a ] -> Stree a
 mkStree [] = Null
 mkStree( x:xs ) = ForkS (mkStree ys) x (mkStree zs)
   where ys = filter (< x) xs
         zs = filter (> x) xs
 
+mkHtree :: [ a ] -> Htree a
+mkHtree xs
+  | null xs = NullH
+  | null ts = ForkH (unwrap x) NullH NullH 
+  | otherwise = ForkH (unwrap x) (mkHtree ys) (mkHtree zs)
+    where m = div (length ts) 2
+          ( ys, zs ) = splitAt m ts
+          ( x, ts ) = splitAt 1 xs 
+
+mkHeap :: (Ord a) => [ a ] -> Htree a
+mkHeap = heapify . mkHtree
+
+heapify :: (Ord a) => Htree a -> Htree a
+heapify NullH = NullH 
+heapify( ForkH x xt yt ) = doPermutationsH x (heapify xt) (heapify yt)
+
+doPermutationsH :: (Ord a) => a -> Htree a -> Htree a -> Htree a
+doPermutationsH x NullH NullH = ForkH x NullH NullH
+doPermutationsH x NullH (ForkH y yt zt) = if x <= y then ForkH x NullH (ForkH y yt zt) else ForkH y NullH (doPermutationsH x yt zt)
+doPermutationsH x (ForkH y yt zt) NullH = if x <= y then ForkH x (ForkH y yt zt) NullH else ForkH y (doPermutationsH x yt zt) NullH
+doPermutationsH x (ForkH y yt zt) (ForkH z mt nt)
+  | x <= min y z = ForkH x (ForkH y yt zt) (ForkH z mt nt)
+  | y <= min x z = ForkH y (doPermutationsH x yt zt) (ForkH z mt nt)
+  | z <= min x y = ForkH z (ForkH y yt zt) (doPermutationsH x mt nt)
+
 isSorted :: (Ord a) => [ a ] -> Bool
-isSorted( x:y:ys ) = x < y && isSorted(y:ys)
+isSorted( x:y:ys ) = if sorted then sorted else error "Not sorted!"
+  where sorted = x <= y && isSorted(y:ys)
 isSorted [ x ] = True
 
 sortS :: (Ord a) => [ a ] -> [ a ]
 sortS = flattenS . mkStree
+
+sortH :: (Ord a) => [ a ] -> [ a ]
+sortH = flattenH . mkHeap
 
 bubbleSort :: (Ord a) => [ a ] -> [ a ]
 bubbleSort [] = []
@@ -73,6 +107,20 @@ flattenS :: Stree a -> [ a ]
 flattenS( Null ) = []
 flattenS( ForkS xt x yt ) = flattenS xt ++ [ x ] ++ flattenS yt
 
+flattenH :: (Ord a) => Htree a -> [ a ]
+flattenH( NullH ) = []
+flattenH( ForkH x yt zt ) = wrap x ++ merge (flattenH yt) (flattenH zt)
+
+merge :: (Ord a) => [ a ] -> [ a ] -> [ a ]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys) =
+  if x <= y then wrap x ++ merge xs (y:ys) else wrap y ++ merge (x:xs) ys
+
 showStree :: (Show a) => Stree a -> String
 showStree Null = "*"
 showStree( ForkS xt x yt ) = "(" ++ show(x) ++ ":" ++ showStree(xt) ++ "," ++ showStree(yt) ++ ")"
+
+showHtree :: (Show a) => Htree a -> String
+showHtree NullH = "*"
+showHtree( ForkH x xt yt ) = "(" ++ show(x) ++ ":" ++ showHtree(xt) ++ "," ++ showHtree(yt) ++ ")"
