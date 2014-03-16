b21c651ac10f57d21c054858e2a2354687cf538a
Laurent FABRE <laurent_psychedelic@hotmail.com>
2014-03-16 16:46:13 +0900

    Added Makefile.

--- C_argc_argv_env-var-ptr/ShowEnv.c
+++ C_argc_argv_env-var-ptr/ShowEnv.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(int ac, char **av[], char** env_var_ptr) {
+int main(int ac, char *av[], char** env_var_ptr) {
     while (NULL != *env_var_ptr) {
         printf("%s\n", *(env_var_ptr++));
     }
     fflush(0);
+    return 0;
 }
