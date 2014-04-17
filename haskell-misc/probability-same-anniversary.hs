83c767a79cb8335cf6f11b759e4b9085345c9d92
Laurent FABRE <laurent_psychedelic@hotmail.com>
2014-04-17 22:31:47 +0900

    Calculation of the probability for groups from 1 person to 60 persons.
    The probability is almost 100% for 60 persons and about 90% for 40 persons!

--- haskell-misc/probability-same-anniversary.hs
+++ haskell-misc/probability-same-anniversary.hs
@@ -1,7 +1,5 @@
 main = do
-  putStrLn( "Probability that at least two people of a group of 20 share the same anniversary: " ++ show p ++ "%" )
-    where p = probSameAnniversary 20
-
+    sequence_ [ putStrLn( "Probability that at least two people of a group of" ++ show( n ) ++ "share the same anniversary: " ++ show( probSameAnniversary n ) ++ "%" ) | n <- [(1::Integer)..(60::Integer)] ]
 -- The probability that all people in a group of N people have a different anniversary is given by 365! / (365-N)! / 365^N
 -- In the program we use the fact that nPool! / (nPool-N)! = (nPool-N+1) * (nPool-N+2) * ... * nPool, to avoid calculating too big numbers
 
